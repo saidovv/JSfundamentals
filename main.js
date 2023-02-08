8c8de5c03c181b9794e62e9d6e75a988d65b21e8
saidovv <116736002+saidovv@users.noreply.github.com>
2023-02-08 15:19:28 +0500

    add all tasks

---
+++ main.js
@@ -0,0 +1,407 @@
+// console.log('Hello World');
+
+
+// let s='5'
+// console.log(+s)
+
+
+// console.log(5>10 ? 'yes': 7==7 ? 2: 0)
+
+
+
+
+
+// 1
+
+// let n=+prompt('enter the number');
+// let k=+prompt('enter the number');
+// console.log( Math.floor (n/k));
+
+
+
+
+// 2
+
+// let n=+prompt('enter the number');
+// let k=+prompt('enter the number');
+// console.log( Math.floor (n%k));
+
+
+
+// 3
+
+// let n=+prompt('enter the number')
+// console.log(n%10)
+
+
+
+// 4
+
+// let n=+prompt('enter the number')
+// console.log(Math.floor (n/10))
+
+
+
+
+// 5
+
+// let n=+prompt('enter the number')
+// console.log(Math.floor (n%100/10))
+
+
+
+// 6
+
+// let n=+prompt('enter the number')
+// let a=n%10;
+// let b=(Math.floor (n%100/10))
+// let c=( Math.floor(n/100));
+// console.log(a+b+c)
+
+
+
+
+// 7
+
+// let a=+prompt('enter the number')
+// let P=4*a
+// console.log(P)
+
+
+
+
+
+
+// 8
+
+// let a=+prompt('enter the number')
+// let b=+prompt('enter the number')
+// let S=a*b;
+// let P=2*(a+b)
+// console.log(S,P)
+
+
+
+
+
+// 9
+
+// let a=+prompt('enter the number')
+// let b=+prompt('enter then number')
+// console.log((a+b)/2)
+
+
+
+
+// 10
+
+// let a=+prompt('enter the number')
+// let b=+prompt('enter the number')
+
+
+
+
+// 11
+
+// let a=+prompt('enter the number')
+// let b=+prompt('enter the number')
+// if (a>b)
+// {
+//     console.log(a)
+
+// }
+// else
+// {
+//     console.log(b)
+// }
+
+
+
+
+
+// 12
+
+// let n=+prompt('enter the number')
+// if (n>=1 && n<=14)
+// {
+//     alert ('First quarter')
+// }
+// else if (n>=15 && n<=29)
+// {
+//     alert ('Second quarter')
+// }
+// else if (n>=30 && n<=45)
+// {
+//     alert ('Third quarter')
+// }
+// else if (n>=46 && n<=59)
+// {
+//     alert ('Fourth quarter')
+// }
+
+
+
+
+// 13
+
+// let n=+prompt('enter the number')
+// if (n==10)
+// {
+//     console.log(true)
+// }
+// else
+// {
+//     console.log(false)
+// }
+
+
+
+
+// 14
+
+// let n=+prompt('enter the number')
+// if (n==0)
+// {
+//     alert(verno)
+// }
+// else
+// {
+//     alert(neverno)
+// }
+
+
+
+
+// 15
+
+// let a=+prompt('enter the number')
+// let b=+prompt('enter the number')
+// let c=+prompt('enter the number')
+// let cnt=0;
+// if (a>0)cnt++;
+// if (b>0)cnt++;
+// if (c>0)cnt++;
+// alert(cnt)
+
+
+
+
+
+// 16
+
+// let n=+prompt('enter the number')
+// if (n>0){
+//     alert (n+1)
+// }
+// else
+// {
+//     alert (n-2)
+// }
+
+
+
+
+
+// 17
+
+// let a=+prompt ('enter the number')
+// let b=+prompt ('enter the number')
+// let c=+prompt ('enter the number')
+// if (a>=b && a>=c)
+// {
+//     alert(a)
+// }
+// else if (b>=a && b>=c)
+// {
+//     alert (b)
+// }
+// else
+// {
+//     alert (c)
+// }
+
+
+
+
+
+
+// 18
+
+// let  a=+prompt('enter the number')
+// if (a==0 || a==2)
+// {
+//     alert (a+7)
+// }
+// else
+// {
+//     alert (a/10)
+// }
+
+
+
+
+
+// 19
+
+// let a=+prompt('enter the number')
+// let b=+prompt ('enter then number')
+// if (a<=1 && b>=3)
+// {
+//     alert (a+b)
+// }
+// else
+// {
+//     alert (a-b)
+// }
+
+
+
+
+
+
+// 20
+
+// let a=+prompt('');
+// let b=+prompt('');
+// let c=+prompt('');
+// let d=+prompt('');
+// let k=Math.abs(a-c);
+// let z=Math.abs(b-d);
+// console.log(k);
+// console.log(z);
+
+
+
+
+// 22
+
+// let month=+prompt('')
+// if (month==12 || month==1 || month==2)
+// {
+//     alert ('Winter')
+// }
+// else if (month==3 || month==4 || month==5)
+// {
+//     alert ('Spring')
+// }
+// else if (month==6 || month==7 || month==8)
+// {
+//     alert ('Summer')
+// }
+// else if (month==9 || month==10 || month==11)
+// {
+//     alert ('Autumn')
+// }
+// else
+// {
+//     alert ('Wrong input')
+// }
+
+
+
+
+
+
+// 23
+
+// let a=+prompt('')
+// if (a==1)
+// {
+//     alert ('Winter')
+// }
+// else if (a==2)
+// {
+//     alert ('Spring')
+// }
+// else if (a==3)
+// {
+//     alert ('Summer')
+// }
+// else if (a==4)
+// {
+//     alert ('Autumn')
+// }
+// else
+// {
+//     alert ('Wrong input')
+// }
+
+
+
+
+
+// 24
+
+// for (let i=0;i<10;i++)
+// {
+//     console.log(i)
+// }
+
+
+
+
+// 25
+
+// let k=prompt('')
+// let n=prompt('')
+// for (let i=0;i<n;i++)
+// {
+//     console.log(k)
+// }
+
+
+
+
+
+
+// 26
+
+// let sum=0;
+// for (let i=200;i<=600;i++)
+// {
+//     sum+=i
+// }
+// console.log(sum)
+
+
+
+
+
+// 27
+
+// let a=+prompt('')
+// let b=+prompt('')
+// for (let i=a;i<=b;i++)
+// if (i%2==0)
+// console.log(i)
+
+
+
+
+
+// 28
+
+// let a=+prompt('')
+// let b=+prompt('')
+// let sum=0
+// for (let i=a;i<=b;i++)
+// {
+//     sum=sum+i
+// }
+// console.log(sum)
+
+
+
+
+
+// 29
+
+// let a=prompt('');
+// let b=prompt('');
+// for (let i=a;i<=b;i++)
+// {
+// if (i*i>a && i*i<b)
+// {
+//     console.log(i*i);
+// }
+// }
